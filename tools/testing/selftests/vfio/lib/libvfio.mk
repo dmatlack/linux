eee39e759e4e9124d2bfb1e750cdfe6b03d8060b
David Matlack <dmatlack@google.com>
2025-03-14 17:15:36 -0700

    vfio: selftests: Add a helper library for VFIO selftests
    Add a basic helper library to be used by VFIO selftests.
    
    The basic unit of the library is struct vfio_pci_device, which
    represents a single PCI device that is bound to the vfio-pci driver. The
    library currently only supports a single device per group and container,
    and VFIO IOMMU types.
    
    The code in this library was heavily based on prior work done by
    Raghavendra Rao Ananta <rananta@google.com>, and the VFIO_ASSERT*()
    macros were written by Vipin Sharma <vipinsh@google.com>.
    
    Separate that Makefile rules for building the library into a separate
    script so that the library can be built by and linked into KVM selftests
    in a subsequent commit.
    
    Signed-off-by: David Matlack <dmatlack@google.com>
    Change-Id: Iff90b4a81888a723296b863e4b3a9338c8f391c9

---
+++ tools/testing/selftests/vfio/lib/libvfio.mk
@@ -0,0 +1,17 @@
+VFIO_DIR := $(selfdir)/vfio
+
+LIBVFIO_C := lib/vfio_pci_device.c
+
+LIBVFIO_O := $(patsubst %.c, $(OUTPUT)/%.o, $(LIBVFIO_C))
+
+LIBVFIO_O_DIRS := $(shell dirname $(LIBVFIO_O) | uniq)
+
+CFLAGS += -I$(VFIO_DIR)/lib/include
+
+$(LIBVFIO_O): $(OUTPUT)/%.o : $(VFIO_DIR)/%.c $(LIBVFIO_O_DIRS)
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c $< -o $@
+
+$(LIBVFIO_O_DIRS):
+	mkdir -p $@
+
+EXTRA_CLEAN += $(LIBVFIO_O)
