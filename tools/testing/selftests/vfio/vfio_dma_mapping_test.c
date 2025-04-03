aaccb9e1a6852adbaff2a88cbfe75aa67d8e0745
Josh Hilke <jrhilke@google.com>
2025-04-03 18:51:48 +0000

    vfio: selftests: Move vfio dma mapping test to their own file
    Move the dma_map_unmap test from vfio_pci_device_test to a new test:
    vfio_dma_mapping_test. We are going to add more complex dma mapping
    tests, so it makes sense to separate this from the vfio pci device
    test which is more of a sanity check for vfio pci functionality.
    
    Signed-off-by: Josh Hilke <jrhilke@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Change-Id: Ia9bff3348416f1056236584e957f14475670c765

---
+++ tools/testing/selftests/vfio/vfio_dma_mapping_test.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0-only
+#include <fcntl.h>
+
+#include <sys/mman.h>
+
+#include <linux/sizes.h>
+#include <linux/vfio.h>
+
+#include <vfio_util.h>
+
+#include "../kselftest_harness.h"
+
+const char *device_bdf;
+
+FIXTURE(vfio_dma_mapping_test)
+{
+	struct vfio_pci_device *device;
+};
+
+FIXTURE_SETUP(vfio_dma_mapping_test)
+{
+	self->device = vfio_pci_device_init(device_bdf, VFIO_TYPE1_IOMMU);
+}
+
+FIXTURE_TEARDOWN(vfio_dma_mapping_test)
+{
+	vfio_pci_device_cleanup(self->device);
+}
+
+TEST_F(vfio_dma_mapping_test, dma_map_unmap)
+{
+	const u64 size = SZ_2M;
+	const u64 iova = SZ_4G;
+	void *mem;
+
+	mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	ASSERT_NE(mem, MAP_FAILED);
+
+	vfio_pci_dma_map(self->device, iova, size, mem);
+	printf("Mapped HVA %p (size 0x%lx) at IOVA 0x%lx\n", mem, size, iova);
+	vfio_pci_dma_unmap(self->device, iova, size);
+
+	ASSERT_TRUE(!munmap(mem, size));
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s segment:bus:device.function\n",
+			argv[0]);
+		return KSFT_FAIL;
+	}
+
+	device_bdf = argv[1];
+
+	return test_harness_run(1, argv);
+}
