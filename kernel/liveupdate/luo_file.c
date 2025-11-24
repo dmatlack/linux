f5f3dd9e538b08cadb21a770c8ca08c204ec5265
David Matlack <dmatlack@google.com>
2025-11-24 23:46:15 +0000

    liveupdate: luo_file: Fix ordering for FLB finish()
    Fix the ordering of when File-Lifecycle Bound (FLB) data finish()
    callbacks are run so that they run after the last file's finish()
    callback and not before.
    
    i.e. The order should be
    
      file1->finish()
      file2->finish()
      file3->finish() // last file
      flb->finish()
    
    rather than
    
      file1->finish()
      file2->finish()
      flb->finish()
      file3->finish() // last file
    
    Fixes: e95b36e9fc80 ("liveupdate: luo_flb: Introduce File-Lifecycle-Bound global state")
    Signed-off-by: David Matlack <dmatlack@google.com>
    Change-Id: I0fce213a8a092df31d29b04c742e04419cdcf272

--- kernel/liveupdate/luo_file.c
+++ kernel/liveupdate/luo_file.c
@@ -639,9 +639,10 @@ static void luo_file_finish_one(struct luo_file_set *file_set,
 	args.file = luo_file->file;
 	args.serialized_data = luo_file->serialized_data;
 	args.retrieved = luo_file->retrieved;
-	luo_flb_file_finish(luo_file->fh);
 
 	luo_file->fh->ops->finish(&args);
+
+	luo_flb_file_finish(luo_file->fh);
 }
 
 /**
